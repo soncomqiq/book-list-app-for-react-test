ae2f7bb0d3877d6c50418d3e19f9fe61063765da
soncomqiq <idgamescs@hotmail.com>
2024-04-29 22:31:43 +0700

    Version 3 (Context)

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import BookEdit from './BookEdit';
+import useBooksContext from '../hooks/use-books-context';
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
+    const { deleteBookById } = useBooksContext();
     const [showEdit, setShowEdit] = useState(false);
 
     const handleDeleteClick = () => {
-        onDelete(book.id);
+        deleteBookById(book.id);
     }
 
-    const handleSubmit = (id, title) => {
-        onEdit(id, title);
+    const handleSubmit = () => {
         setShowEdit(false);
     }
 
@@ -18,13 +19,13 @@ function BookShow({ book, onDelete, onEdit }) {
     }
 
     let content = <h3>{book.title}</h3>
-    if(showEdit) {
+    if (showEdit) {
         content = <BookEdit onSubmit={handleSubmit} book={book} />
     }
 
     return (
         <div className="book-show">
-            <img 
+            <img
                 alt="books"
                 src={`https://picsum.photos/seed/${book.id}/300/200`}
             />
